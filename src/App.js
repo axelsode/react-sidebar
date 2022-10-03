0695aedeed549f852c55a5156f8389d72efcf8dc
Axl <axel.soderberg1996@gmail.com>
2022-10-03 12:10:35 +0200

    Done, basic navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Info from './pages/Info';
+import Games from './pages/Games';
+import Future from './pages/Future';
+import Support from './pages/Support';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/future" element={<Future />} />
+          <Route path="/support" element={<Support />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/NavbarData.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+
+
+export const NavbarData = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    className: 'nav-text',
+  },
+  {
+    title: 'Info',
+    path: '/info',
+    icon: <AiIcons.AiFillHome />,
+    className: 'nav-text',
+  },
+  {
+    title: 'Games',
+    path: '/games',
+    icon: <AiIcons.AiFillHome />,
+    className: 'nav-text',
+  },
+  {
+    title: 'Future',
+    path: '/future',
+    icon: <AiIcons.AiFillHome />,
+    className: 'nav-text',
+  },
+  {
+    title: 'Support',
+    path: '/support',
+    icon: <AiIcons.AiFillHome />,
+    className: 'nav-text',
+  },
+];
